e98fa5dc33645a95d5043697a7c2d0137862b189
Lukas Obermann <lukas.obermann@icloud.com>
2023-06-07 17:08:51 +0200

    build: use optolith-tsjsonschemamd to generate schema from typescript definition

---
+++ otjsmd.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+import { dirname, join } from "node:path"
+import { fileURLToPath } from "node:url"
+import { jsonSchema } from "optolith-tsjsonschemamd/renderers"
+
+const rootDir = dirname(fileURLToPath(import.meta.url))
+const sourceDir = join(rootDir, "src")
+const schemaDir = join(rootDir, "schema")
+
+/** @type {import("optolith-tsjsonschemamd").GeneratorOptions} */
+export default {
+  sourceDir: sourceDir,
+  outputs: [
+    {
+      targetDir: schemaDir,
+      renderer: jsonSchema({ spec: "Draft_07" }),
+    }
+  ],
+  clean: true,
+  verbose: false
+}
